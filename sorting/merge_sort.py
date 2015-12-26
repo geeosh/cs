fa8a0b6f1db75cc03352189298cbd58eb3e434ee
Josh Schwartzman <geeosh@gmail.com>
2015-12-26 07:35:01 -0700

    Finish merge sort algorithm

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -2,18 +2,19 @@
 # Merge Sort class that allows for O(n log n) sorting of items, using a divide-and-conquer strategy
 # There is some memory overhead as it uses two buffers to store items when they are merged
 
+from collections import deque
+
 class MergeSort:
 
   def __init__(self, items):
     self.items = items
-    self.sorted = []
 
   # Sort items
   def sort(self, low = None, high = None):
     if(low == None):
       low = 0
     if(high == None):
-      high = len(self.items)
+      high = len(self.items) - 1
     
     if(low < high):
       middle = (low + high) / 2
@@ -23,31 +24,32 @@ class MergeSort:
 
   # Merge together results
   def _merge(self, low, middle, high):
-    # Buffers are expected to be used as FIFO queues
-    buffer1 = []; buffer2 = []
+    # Buffers hold elements to merge
+    # They are FIFO queues, deque is best data structure in Python for this
+    buffer1 = deque(); buffer2 = deque()
 
-    print "Low %d Middle %d High %d" % (low, middle, high)
-
-    for i in range(low,middle):
-      print i
+    # For loops with range are not inclusive of last element, so need to add 1
+    for i in range(low, middle + 1):
       buffer1.append(self.items[i])
 
-    for i in range(middle + 1, high):
-      print i
+    for i in range(middle + 1, high + 1):
       buffer2.append(self.items[i])
 
+    i = low
     while(len(buffer1) > 0 and len(buffer2) > 0):
       if(buffer1[0] <= buffer2[0]):
-        self.sorted.append(buffer1.pop(0))
+        self.items[i] = buffer1.popleft()
       else:
-        self.sorted.append(buffer2.pop(0))
+        self.items[i] = buffer2.popleft()
+      i += 1
 
     while(len(buffer1) > 0):
-      self.sorted.append(buffer1.pop(0))
+      self.items[i] = buffer1.popleft()
+      i += 1
 
     while(len(buffer2) > 0):
-      self.sorted.append(buffer2.pop(0))
-
+      self.items[i] = buffer2.popleft()
+      i += 1
 
 import random
 
@@ -63,4 +65,4 @@ print "Unsorted items: " + ", ".join(str(i) for i in items)
 ms = MergeSort(items)
 ms.sort()
 
-print "Sorted items: " + ", ".join(str(i) for i in ms.sorted)
+print "Sorted items: " + ", ".join(str(i) for i in ms.items)

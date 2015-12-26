e2527f3a215cb5af4d119193859997ace801f275
Josh Schwartzman <geeosh@gmail.com>
2015-12-26 08:22:13 -0700

    QuickSort implemented

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -9,6 +9,31 @@ class QuickSort:
   def __init__(self, items):
     self.items = items
 
+  def sort(self, low = None, high = None):
+    if(low == None):
+      low = 0
+    if (high == None):
+      high = len(self.items) - 1
+
+    if(high - low > 0):
+      pivot = self.partition(low, high)
+      self.sort(low, pivot - 1)
+      self.sort(pivot + 1, high)
+
+  def partition(self, low, high):
+    pivot = high
+    first_high = low
+
+    for i in range(low, high):
+      if(self.items[i] < self.items[pivot]):
+        # Swap items
+        self.items[i], self.items[first_high] = self.items[first_high], self.items[i]
+        first_high += 1
+
+    self.items[pivot], self.items[first_high] = self.items[first_high], self.items[pivot]
+
+    return first_high
+
 import random
 
 print "Initialize QuickSort with array"
